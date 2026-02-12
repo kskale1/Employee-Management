d8bec30f11f6f5c6ca2fc7f50135e07b97602262
kskale1 <kskale03@gmail.com>
2026-02-12 08:42:50 +0530

    Rename authContext to AuthContext

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useContext, useEffect, useState } from "react";
 
 const userContext = createContext();
 
-const authContext = ({ children }) => {
+const AuthContext = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -54,4 +54,4 @@ const authContext = ({ children }) => {
 };
 
 export const useAuth = () => useContext(userContext);
-export default authContext;
+export default AuthContext;
